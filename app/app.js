162aecc0ac28688018a4eb829086f6fc8fb5d96c
Andy Liu <jiananliu@umass.edu>
2016-11-09 20:10:11 -0500

    "Starting the process of building with React. Setup app.js and Starting to build notification page."

--- app/app.js
+++ app/app.js
@@ -1,1 +1,36 @@
-/* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MailBox from './components/mailbox';
+import { IndexRoute, Router, Route, browserHistory } from 'react-router';
+
+// Fake Home Page
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <p>I am Fake</p>
+    );
+  }
+}
+
+class NotificationPage extends React.Component {
+  render() {
+    return <MailBox />;
+  }
+}
+
+class App extends React.Component {
+    render() {
+      return (
+        <div>{this.props.children}</div>
+      )
+    }
+  }
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={LandingPage} />
+      <Route path="notification" component={NotificationPage} />
+    </Route>
+  </Router>
+),document.getElementById('thebody'));
