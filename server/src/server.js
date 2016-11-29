424c72b10ca02fb0a9de565a6c9d753536261289
Michael Schmittlein <schmitty-32@hotmail.com>
2016-11-28 22:16:55 -0500

    started server.js

---
+++ server/src/server.js
@@ -0,0 +1,32 @@
+var bodyParser = require('body-parser');
+var database = require("./database.js");
+var readDocument = database.readDocument;
+var validate = require('express-jsonschema').validate;
+var writeDocument = database.writeDocument;
+var addDocument = database.addDocument;
+
+// Imports the express Node module.
+var express = require('express');
+// Creates an Express server.
+var app = express();
+
+app.use(bodyParser.text());
+// Support receiving text in HTTP request bodies
+app.use(bodyParser.text());
+// Support receiving JSON in HTTP request bodies
+app.use(bodyParser.json());
+// You run the server from `server`, so `../client/build` is `server/../client/build`.
+// '..' means "go up one directory", so this translates into `client/build`!
+app.use(express.static('../client/build'));
+/**
+* Translate JSON Schema Validation failures into error 400s.
+*/
+app.use(function(err, req, res, next) {
+  if (err.name === 'JsonSchemaValidation') {
+    // Set a bad request http response status
+    res.status(400).end();
+  } else {
+    // It's some other sort of error; pass it to next error middleware handler
+    next(err);
+  }
+});
