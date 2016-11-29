2d5e7df7729d836addd1f1337eef56dc4e7c65da
Michael Schmittlein <schmitty-32@hotmail.com>
2016-11-28 22:57:32 -0500

    .eslintrc, package.json, readme.md

--- server/src/database.js
+++ server/src/database.js
@@ -291,58 +291,55 @@ function JSONClone(obj) {
  * Doesn't do any tricky document joins, as we will cover that in the latter
  * half of the course. :)
  */
- function readDocument(collection, id) {
-   // Clone the data. We do this to model a database, where you receive a
-   // *copy* of an object and not the object itself.
-   var collectionObj = data[collection];
-   if (!collectionObj) {
-     throw new Error(`Object collection ${collection} does not exist in the database!`);
-   }
-   var obj = collectionObj[id];
-   if (obj === undefined) {
-     throw new Error(`Object ${id} does not exist in object collection ${collection} in the database!`);
-   }
-   return JSONClone(data[collection][id]);
- }
- module.exports.readDocument = readDocument;
-
-export function readDocumentNoId(collection){
-  return JSONClone(data[collection])
+function readDocument(collection, id) {
+  // Clone the data. We do this to model a database, where you receive a
+  // *copy* of an object and not the object itself.
+  var collectionObj = data[collection];
+  if (!collectionObj) {
+    throw new Error(`Object collection ${collection} does not exist in the database!`);
+  }
+  var obj = collectionObj[id];
+  if (obj === undefined) {
+    throw new Error(`Object ${id} does not exist in object collection ${collection} in the database!`);
+  }
+  return JSONClone(data[collection][id]);
 }
+module.exports.readDocument = readDocument;
+
 /**
  * Emulates writing a "document" to a NoSQL database.
  */
- function writeDocument(collection, changedDocument) {
-   var id = changedDocument._id;
-   if (id === undefined) {
-     throw new Error(`You cannot write a document to the database without an _id! Use AddDocument if this is a new object.`);
-   }
-   // Store a copy of the object into the database. Models a database's behavior.
-   data[collection][id] = JSONClone(changedDocument);
-   // Update our 'database'.
-   updated = true;
- }
- module.exports.writeDocument = writeDocument;
+function writeDocument(collection, changedDocument) {
+  var id = changedDocument._id;
+  if (id === undefined) {
+    throw new Error(`You cannot write a document to the database without an _id! Use AddDocument if this is a new object.`);
+  }
+  // Store a copy of the object into the database. Models a database's behavior.
+  data[collection][id] = JSONClone(changedDocument);
+  // Update our 'database'.
+  updated = true;
+}
+module.exports.writeDocument = writeDocument;
 
 /**
  * Adds a new document to the NoSQL database.
  */
- function addDocument(collectionName, newDoc) {
-   var collection = data[collectionName];
-   var nextId = Object.keys(collection).length;
-   if (newDoc.hasOwnProperty('_id')) {
-     throw new Error(`You cannot add a document that already has an _id. addDocument is for new documents that do not have an ID yet.`);
-   }
-   while (collection[nextId]) {
-     nextId++;
-   }
-   newDoc._id = nextId;
-   writeDocument(collectionName, newDoc);
-   return newDoc;
- }
- module.exports.addDocument = addDocument;
+function addDocument(collectionName, newDoc) {
+  var collection = data[collectionName];
+  var nextId = Object.keys(collection).length;
+  if (newDoc.hasOwnProperty('_id')) {
+    throw new Error(`You cannot add a document that already has an _id. addDocument is for new documents that do not have an ID yet.`);
+  }
+  while (collection[nextId]) {
+    nextId++;
+  }
+  newDoc._id = nextId;
+  writeDocument(collectionName, newDoc);
+  return newDoc;
+}
+module.exports.addDocument = addDocument;
 
- /**
+/**
  * Deletes a document from an object collection.
  */
 function deleteDocument(collectionName, id) {
