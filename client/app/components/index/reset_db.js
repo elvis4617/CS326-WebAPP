9e9fd5f40229662f42ed7a2c50c6a7313476fa34
Michael Schmittlein <schmitty-32@hotmail.com>
2016-11-29 21:19:44 -0500

    added reset database to server

--- client/app/components/index/reset_db.js
+++ client/app/components/index/reset_db.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {resetDatabase} from '../../database'
 
 export default class Reset_DB extends React.Component{
-  render(){
-    return(
-      <a href="#" onClick={() => {
-              resetDatabase();
-              window.alert("Database reset! Refreshing the page now...");
-              window.location.reload(true);
-            }}>Reset DB</a>
-    );
-  }
+  render() {
+    return (
+      <button className="btn btn-default" type="button" onClick={() => {
+          var xhr = new XMLHttpRequest();
+          xhr.open('POST', '/resetdb');
+          xhr.addEventListener('load', function() {
+            window.alert("Database reset! Refreshing the page now...");
+            document.location.reload(false);
+          });
+          xhr.send();
+        }}>Reset Mock DB</button>
+      );
+    }
 }
--- server/src/server.js
+++ server/src/server.js
@@ -30,3 +30,12 @@ app.use(function(err, req, res, next) {
     next(err);
   }
 });
+
+// Reset database.
+  app.post('/resetdb', function(req, res) {
+  console.log("Resetting database...");
+  // This is a debug route, so don't do any validation.
+  database.resetDatabase();
+  // res.send() sends an empty response with status code 200
+  res.send();
+  });
