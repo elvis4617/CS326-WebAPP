1a4cf9a0d7fbf93334603e5f3503cd6dd36de116
Michael Schmittlein <schmitty-32@hotmail.com>
2016-11-29 21:36:32 -0500

    added sendXHR to client server, added errorBanner

--- client/app/server.js
+++ client/app/server.js
@@ -1,5 +1,71 @@
 import {readDocument, writeDocument, addDocument} from './database.js';
 
+// token is for 'id: 2'
+var token = 'eyJpZCI6Mn0='; // <-- Put your base64'd JSON token here
+/**
+* Properly configure+send an XMLHttpRequest with error handling,
+* authorization token, and other needed properties.
+*/
+function sendXHR(verb, resource, body, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(verb, resource);
+  xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+  // The below comment tells ESLint that ToGatherError is a global.
+  // Otherwise, ESLint would complain about it! (See what happens in Atom if
+  // you remove the comment...)
+  /* global ToGatherError */
+  // Response received from server. It could be a failure, though!
+  xhr.addEventListener('load', function() {
+    var statusCode = xhr.status;
+    var statusText = xhr.statusText;
+    if (statusCode >= 200 && statusCode < 300) {
+      // Success: Status code is in the [200, 300) range.
+      // Call the callback with the final XHR object.
+      cb(xhr);
+    } else {
+      // Client or server error.
+      // The server may have included some response text with details concerning
+      // the error.
+      var responseText = xhr.responseText;
+      ToGatherError('Could not ' + verb + " " + resource + ": Received " +
+      statusCode + " " + statusText + ": " + responseText);
+    }
+  });
+  // Time out the request if it takes longer than 10,000
+  // milliseconds (10 seconds)
+  xhr.timeout = 10000;
+  // Network failure: Could not connect to server.
+  xhr.addEventListener('error', function() {
+    ToGatherError('Could not ' + verb + " " + resource +
+    ": Could not connect to the server.");
+  });
+  // Network failure: request took too long to complete.
+  xhr.addEventListener('timeout', function() {
+    ToGatherError('Could not ' + verb + " " + resource +
+    ": Request timed out.");
+  });
+  switch (typeof(body)) {
+    case 'undefined':
+    // No body to send.
+    xhr.send();
+    break;
+    case 'string':
+    // Tell the server we are sending text.
+    xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+    xhr.send(body);
+    break;
+    case 'object':
+    // Tell the server we are sending JSON.
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    // Convert body into a JSON string.
+    xhr.send(JSON.stringify(body));
+    break;
+    default:
+    throw new Error('Unknown body type: ' + typeof(body));
+  }
+}
+
+
 export function getRequestItem(reqId, cb){
   var req = readDocument('requestItems', reqId);
   var value = {log : req};
